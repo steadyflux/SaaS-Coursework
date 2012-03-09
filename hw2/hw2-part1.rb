0d2dff223eaf402778b248510f5aaee4772a7cd5
Katie Baynes <kathleen.baynes@nasa.gov>
2012-03-08 23:16:23 -0500

    updating part 1

--- hw2/hw2-part1.rb
+++ hw2/hw2-part1.rb
@@ -1,4 +1,4 @@
-require 'test/unit'
+# require 'test/unit'
 
 class Numeric
   @@currencies = {'dollar' => 1, 'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
@@ -33,82 +33,82 @@ end
 
 module Enumerable
   def palindrome?
-    self == self.reverse if self.is_a?(Array)
+    self.to_a == self.to_a.reverse
   end
 end
 
-class HW2Part1Test < Test::Unit::TestCase
-
-  def test_to_dollars_singular
-    base = Random.rand(100)
-    assert base.yen == base * 0.013
-    assert base.euro == base * 1.292
-    assert base.rupee == base * 0.019
-    assert base.dollar == base
-  end
-  
-  def test_to_dollars_plural
-    base = Random.rand(100)
-    assert base.yens == base * 0.013
-    assert base.euros == base * 1.292
-    assert base.rupees == base * 0.019
-    assert base.dollar == base
-  end
-    
-  def test_singular_in
-    base = Random.rand(100)
-    assert base.yen.in(:euro) == base * 0.013 / 1.292
-    assert base.euro.in(:rupee) == base * 1.292 / 0.019
-    assert base.rupee.in(:rupee) == base * 0.019 / 0.019   
-    assert base.dollar.in(:yen) == base / 0.013
-  end
-
-  def test_plural_in
-    base = Random.rand(100)
-    assert base.yens.in(:euros) == base * 0.013 / 1.292
-    assert base.euros.in(:rupees) == base * 1.292 / 0.019
-    assert base.yens.in(:euros) == base * 0.013 / 1.292
-    assert base.rupees.in(:rupees) == base * 0.019 / 0.019
-    assert base.dollars.in(:yens) == base / 0.013
-    assert base.rupees.in(:dollars) == base * 0.019
-  end
-
-  def test_unknown_to_dollars
-    begin
-      100.kronor
-      assert false
-    rescue
-    end
-  end
-  
-  def test_unknown_in
-    begin
-      100.kronor.in(:dollars)
-      assert false
-    rescue
-    end
-  end
-  
-  def test_bad_string_palindrome
-    assert !"foo".palindrome?
-  end
-  
-  def test_good_string_palindrome
-    assert "A man, a plan, a canal -- Panama".palindrome?
-  end
-  
-  def test_bad_array_palindrome
-    assert ![3,4].palindrome?
-    assert ![3,4,5].palindrome?
-  end
-  
-  def test_good_array_palindrome
-    assert [3].palindrome?
-    assert [3,4,3].palindrome?
-    assert [1,2,3,2,1].palindrome?
-  end
-  
-  def test_hash_palindrome
-    {"hash" => "value"}.palindrome?
-  end
-end
+# class HW2Part1Test < Test::Unit::TestCase
+# 
+#   def test_to_dollars_singular
+#     base = Random.rand(100)
+#     assert base.yen == base * 0.013
+#     assert base.euro == base * 1.292
+#     assert base.rupee == base * 0.019
+#     assert base.dollar == base
+#   end
+#   
+#   def test_to_dollars_plural
+#     base = Random.rand(100)
+#     assert base.yens == base * 0.013
+#     assert base.euros == base * 1.292
+#     assert base.rupees == base * 0.019
+#     assert base.dollar == base
+#   end
+#     
+#   def test_singular_in
+#     base = Random.rand(100)
+#     assert base.yen.in(:euro) == base * 0.013 / 1.292
+#     assert base.euro.in(:rupee) == base * 1.292 / 0.019
+#     assert base.rupee.in(:rupee) == base * 0.019 / 0.019   
+#     assert base.dollar.in(:yen) == base / 0.013
+#   end
+# 
+#   def test_plural_in
+#     base = Random.rand(100)
+#     assert base.yens.in(:euros) == base * 0.013 / 1.292
+#     assert base.euros.in(:rupees) == base * 1.292 / 0.019
+#     assert base.yens.in(:euros) == base * 0.013 / 1.292
+#     assert base.rupees.in(:rupees) == base * 0.019 / 0.019
+#     assert base.dollars.in(:yens) == base / 0.013
+#     assert base.rupees.in(:dollars) == base * 0.019
+#   end
+# 
+#   def test_unknown_to_dollars
+#     begin
+#       100.kronor
+#       assert false
+#     rescue
+#     end
+#   end
+#   
+#   def test_unknown_in
+#     begin
+#       100.kronor.in(:dollars)
+#       assert false
+#     rescue
+#     end
+#   end
+#   
+#   def test_bad_string_palindrome
+#     assert !"foo".palindrome?
+#   end
+#   
+#   def test_good_string_palindrome
+#     assert "A man, a plan, a canal -- Panama".palindrome?
+#   end
+#   
+#   def test_bad_array_palindrome
+#     assert ![3,4].palindrome?
+#     assert ![3,4,5].palindrome?
+#   end
+#   
+#   def test_good_array_palindrome
+#     assert [3].palindrome?
+#     assert [3,4,3].palindrome?
+#     assert [1,2,3,2,1].palindrome?
+#   end
+#   
+#   def test_hash_palindrome
+#     {"hash" => "value"}.palindrome?
+#   end
+# end
